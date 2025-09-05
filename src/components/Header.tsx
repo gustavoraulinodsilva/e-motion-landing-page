42d0919a05ce1594259316bd0fdb0c7cde929153
Gustavo Raulino da Silva <gustavoraulinodasilva@gmail.com>
2025-09-04 22:13:33 -0300

    Iniciado a contrução do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,41 @@
-'use client';
+"use client";
 
-import React from 'react';
+import React from "react";
 
 function Header() {
-  return (
-    <header className="bg-blue-600 text-white p-4">
-      <h1 className="text-2xl font-bold">My Application</h1>
-    </header>
-  );
+    return (
+        <header className="fixed w-full">
+            <nav className="flex items-center justify-between px-6 py-4">
+                <h1>E-motion</h1>
+                <ul className="flex items-center space-x-4">
+                    <li>
+                        <a href="">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="">Desafio</a>
+                    </li>
+                    <li>
+                        <a href="#">Solução</a>
+                    </li>
+                    <li>
+                        <a href="">Coleta</a>
+                    </li>
+                    <li>
+                        <a href="">Curiosidades</a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <select name="" id="">
+                            <option value="pt-br">PT-BR</option>
+                            <option value="en">EN</option>
+                            <option value="es">ES</option>
+                        </select>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
